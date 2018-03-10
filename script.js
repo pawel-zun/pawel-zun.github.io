57e404eece0e1d1bd4b7cb4477e16d580873c8d6
Pawel Zun <pzun90@gmail.com>
2018-03-10 19:01:37 +0100

    Add scrolling library

---
+++ script.js
@@ -0,0 +1,13 @@
+$(".sections__wrapper").onepage_scroll({
+	sectionContainer: "section",
+	easing: "ease-in-out",
+	animationTime: 1000,
+	pagination: true,
+	updateURL: false,
+	beforeMove: function (index) { },
+	afterMove: function (index) { },
+	loop: true,
+	keyboard: true,
+	responsiveFallback: false,
+	direction: "vertical" 
+});

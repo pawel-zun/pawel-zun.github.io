002552915eb6097a9fc16a9d06392fa845992ea2
Pawel Zun <pzun90@gmail.com>
2018-03-11 18:18:48 +0100

    Finish carousel in works section

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,7 +34,8 @@ module.exports = function(grunt) {
 				bsFiles: {
 					src: [
 						'*.css',
-						'*.html'
+						'*.html',
+						'*.js'
 					]
 				},
 				options: {
--- works__carousel.js
+++ works__carousel.js
@@ -1,11 +1,43 @@
 $(function() {
   const carouselContainer = $('.works__carousel .works__slides');
-	setInterval(changeSlide, 4000);
-  function changeSlide() { carouselContainer.animate({'marginLeft': '-100%'}, 500, moveFirstSlide) };
-	function moveFirstSlide() {
-    let firstItem = carouselContainer.find('.works__container:first');
-    let lastItem = carouselContainer.find('.works__container:last');
-		lastItem.after(firstItem);
-    carouselContainer.css({marginLeft: 0});
-	}
+  const carouselArea = $('.works__main');
+  const btnLeft = $('#btn-left');
+  const btnRight = $('#btn-right');
+  let direction = '';
+
+  function moveFirstSlide() {
+    const firstItem = carouselContainer.find('.works__container:first');
+    const lastItem = carouselContainer.find('.works__container:last');
+    if (direction === 'left') {
+      lastItem.after(firstItem);
+      carouselContainer.css({ marginLeft: 0 });
+    } else if (direction === 'right') {
+      firstItem.before(lastItem);
+      carouselContainer.css({ 'marginLeft': '-100%' });
+    }
+  }
+
+  function goLeft() { 
+    direction = 'left';
+    carouselContainer.animate({ 'marginLeft': '-100%' }, 500, moveFirstSlide)
+  };
+
+  function goRight() {
+    direction = 'right';
+    moveFirstSlide();
+    carouselContainer.animate({ 'marginLeft': 0 }, 500);
+    direction = 'left';
+  }
+
+  let keepGoing = setInterval(goLeft, 4000);
+
+  btnLeft.on('click', goLeft);
+  btnRight.on('click', goRight);
+  
+  carouselArea.on('mouseenter', function() {
+    clearInterval(keepGoing);
+  });
+  carouselArea.on('mouseleave', function () {
+    keepGoing = setInterval(goLeft, 4000);
+  });
 });

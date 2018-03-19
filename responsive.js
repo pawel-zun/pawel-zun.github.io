fe4e19efd3899547139d23bbcecbaf7727fcf75d
Pawel Zun <pzun90@gmail.com>
2018-03-19 23:47:02 +0100

    Add resposivity for tablets

--- responsive.js
+++ responsive.js
@@ -1,9 +1,16 @@
+function hideAll() {
+  toggleFullHeader();
+  $('#card__burger').prop('checked', false);
+}
+
 function toggleFullHeader(params) {
   $('.card').toggleClass('card--full');
   $('.card__wrapper').toggleClass('card__wrapper--full');
   $('.card__main').toggleClass('card__main--full');
   $('.card__name').toggleClass('card__name--full');
-  $('.navigation__list').click(toggleFullHeader);
+  $('.card__photo').toggleClass('card__photo--full');
+  $('.stripes-group').toggleClass('stripes-group--full');
+  $('.navigation__list').click(hideAll);
 }
 
 
